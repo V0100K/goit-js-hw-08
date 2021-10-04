6e4149ff8b86dc5d9133ae127c371df4f6c27530
V0100K <39759882+V0100K@users.noreply.github.com>
2021-10-04 15:08:29 +0000

    Deploying to gh-pages from @ V0100K/goit-js-hw-08@e6125595dde2cb48d692e2cae83056d26caab144 🚀

--- 02-video.43e7bca6.js
+++ 02-video.43e7bca6.js
@@ -4,4 +4,4 @@ var t=arguments[3],n="Expected a function",e=NaN,i="[object Symbol]",r=/^\s+|\s+
 },{}],"iiAF":[function(require,module,exports) {
 "use strict";var e=t(require("lodash.throttle"));function t(e){return e&&e.__esModule?e:{default:e}}const r=document.querySelector("iframe"),o=new Vimeo.Player(r);o.on("timeupdate",(0,e.default)(e=>{localStorage.setItem("videoplayer-current-time",e.seconds)},1e3));let l=localStorage.getItem("videoplayer-current-time")||0;o.setCurrentTime(l);
 },{"lodash.throttle":"FNyO"}]},{},["iiAF"], null)
-//# sourceMappingURL=/goit-js-hw-08/02-video.4ba38eab.js.map
+//# sourceMappingURL=/goit-js-hw-08/02-video.43e7bca6.js.map
